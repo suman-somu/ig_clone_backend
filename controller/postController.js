c8d470faf84b5c6f4bb8a82c5b05be64d804764a
Suman Sahoo <ssamsomu@gmail.com>
2023-07-05 22:58:56 +0530

    postid logic implemented

--- controller/postController.js
+++ controller/postController.js
@@ -7,18 +7,26 @@ const Post = require("../models/postModel");
 const post = async (req, res) => {
   // console.log(req);
   try {
-    
     //make postid
     //the images are stored, retreive the path
-    //store caption 
+    //store caption
 
-    const { caption } = req.body ;
+    const currentDate = new Date();
+    const year = currentDate.getFullYear().toString();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-based
+    const day = currentDate.getDate().toString().padStart(2, "0");
+    const hours = currentDate.getHours().toString().padStart(2, "0");
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+    const seconds = currentDate.getSeconds().toString().padStart(2, "0");
+
+    const postid = Number ( year + month + day + hours + minutes + seconds );
+    const { caption } = req.body;
     const filepath = req.file.path;
-    const postid = 1;
+
     const createdpost = new Post({
-        postid: postid,
-        caption: caption,
-        filepath: filepath,
+      postid: postid,
+      caption: caption,
+      filepath: filepath,
     });
 
     const savepost = await createdpost.save();
@@ -28,7 +36,6 @@ const post = async (req, res) => {
       status: "success",
       message: "user saved successfully",
     });
-
   } catch (e) {
     res.status(500).send({
       status: "failure",
